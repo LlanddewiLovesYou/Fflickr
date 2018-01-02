0e8b120650dae99d1cf31b3ec57bfd7cf6a0e73b
Ian Del Duca <ianmdelduca@gmail.com>
2018-01-02 18:03:33 -0500

    end of day 1, there is a weird redux-thunk issues

--- frontend/fflickr.js
+++ frontend/fflickr.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/Root';
 import * as SessionAPI from "./util/session_api_util";
-
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
+  const store = configureStore();
   // window.login = SessionAPI.login;
   // window.logout = SessionAPI.logout;
   // window.newUser = SessionAPI.newUser;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to Fflickr</h1>, root);
 });

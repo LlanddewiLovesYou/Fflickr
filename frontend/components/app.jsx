c38b3f973cfe6f8d4e18d65edc8841b7dab47c6e
Ian Del Duca <ianmdelduca@gmail.com>
2018-01-03 11:30:23 -0500

    finished session form and session form container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import GreetingContainer from './greeting_container';
 import Greeting from './greeting';
+import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+import SessionFormContainer from './session_form_container';
 
 const App = () => (
+<main>
   <div>
     <h1>Fflickr</h1>
     <GreetingContainer/>
   </div>
+  <Route path="/login" component={SessionFormContainer} />
+  <Route path="/signup" component={SessionFormContainer} />
+</main>
 );
 
 export default App;

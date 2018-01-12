69b7947908ac1014a5e4a3127a5bd4092746aefc
Ian Del Duca <ianmdelduca@gmail.com>
2018-01-12 12:06:09 -0500

    yet another typo

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -26,7 +26,7 @@ const SplashPage = (props) => {
 
             <div className="inspiration-wrapper">
               <h1 className='inspiration-text'> Find your inspiration. </h1>
-              <h1 className='splash-text'> Join the Flickr community, home to tens of <br></br> photos and 2 groups. </h1>
+              <h1 className='splash-text'> Join the Fflickr community, home to tens of <br></br> photos and 2 groups. </h1>
               {button}
             </div>
           <Footer/>
